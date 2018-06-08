370ede8f696181c45377ebf5578aa3b363e4402e
Thiago Santos <thia.mdossantos@gmail.com>
2018-06-08 16:19:02 -0300

    test: test the API

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source 'https://rubygems.org'
 
 gem 'grape'
 gem 'rack', '~> 2.0'
+gem 'rack-test', '~> 1.0'
 gem 'rspec', '~> 3.7'
 gem 'rubocop'
--- spec/holidays_spec.rb
+++ spec/holidays_spec.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-RSpec.describe 'test' do
-  it 'it passs' do
-    expect(10).to eq(10)
+require 'spec_helper'
+require 'rack/test'
+require_relative '../app/holidays'
+
+describe Holidays::API do
+  include Rack::Test::Methods
+
+  def app
+    Holidays::API
+  end
+
+  context 'GET /hello' do
+    it 'returns hello world' do
+      get '/hello'
+      expect(last_response.status).to eq(200)
+      actual = JSON.parse(last_response.body)
+      expected = {'hello' => 'world'}
+
+      expect(actual).to eq(expected)
+    end
   end
 end
