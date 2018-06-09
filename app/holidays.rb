688975015a46638fe98fc2d0fd80bb4cb67d0a57
Thiago Santos <thia.mdossantos@gmail.com>
2018-06-09 18:55:19 -0300

    feat: load active record configuration

--- app/holidays.rb
+++ app/holidays.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
+require 'yaml'
+require 'erb'
 require 'grape'
 require 'sqlite3'
 require 'active_record'
 require_relative './models/holiday'
 
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: 'db/development.sqlite3'
-)
+ActiveRecord::Base.configurations = YAML.load(ERB.new(IO.read('db/config.yml')).result)
+ActiveRecord::Base.establish_connection(ENV.fetch('ENV', 'development').to_sym)
 
 module Holidays
   class API < Grape::API
