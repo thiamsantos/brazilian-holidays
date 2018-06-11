d631c5c171eac3a9e3342d20f460c5052520e5e4
Thiago Santos <thia.mdossantos@gmail.com>
2018-06-11 11:19:39 -0300

    feat: add list all and filter by year

--- app/holidays.rb
+++ app/holidays.rb
@@ -16,15 +16,54 @@ unless ActiveRecord::Base.connected?
 end
 
 module Holidays
+  class Service
+    def self.all
+      Holiday
+        .select(:occurs_at, :name)
+        .map { |holiday| { name: holiday[:name], date: holiday[:occurs_at] } }
+    end
+
+    def self.filter_by_year(year)
+      start_date = Date.new(year, 1, 1)
+      end_date = Date.new(year, -1, -1)
+
+      Holiday
+        .select(:occurs_at, :name)
+        .where('occurs_at >= ? and occurs_at <= ?', start_date, end_date)
+        .map do |holiday|
+          { name: holiday[:name], date: holiday[:occurs_at] }
+        end
+    end
+  end
+
   class API < Grape::API
     format :json
 
-    get :hello do
-      { hello: 'world' }
-    end
+    resource :holidays do
+      get do
+        Service.all
+      end
 
-    get :all do
-      Holiday.all
+      resource :year do
+        route_param :year_id do
+          get do
+            year = params[:year_id].to_i
+
+            if year.zero?
+              message = "`#{params[:year_id]}` is not a valid year!"
+              error!({ error: 'invalid', message: message }, 400)
+            end
+
+            holidays = Service.filter_by_year(year)
+
+            if holidays.empty?
+              error!({ error: 'not_found', message: 'No holidays found!' }, 404)
+            end
+
+            holidays
+          end
+        end
+      end
     end
   end
 end
--- spec/holidays_spec.rb
+++ spec/holidays_spec.rb
@@ -2,6 +2,7 @@
 
 require 'spec_helper'
 require 'rack/test'
+require 'timecop'
 require_relative '../app/holidays'
 
 describe Holidays::API do
@@ -11,21 +12,68 @@ describe Holidays::API do
     Holidays::API
   end
 
-  context 'GET /hello' do
-    it 'returns hello world' do
-      get '/hello'
+  before do
+    Timecop.freeze(Date.new(2018, 6, 11))
+  end
+
+  after do
+    Timecop.return
+  end
+
+  context 'GET /holidays' do
+    it 'shoould return all holidays' do
+      today = Date.today
+      name = 'Holiday'
+      Holiday.create(name: name, occurs_at: today)
+
+      get '/holidays'
       expect(last_response.status).to eq(200)
       actual = JSON.parse(last_response.body)
-      expected = { 'hello' => 'world' }
+      expected = [{ 'name' => name, 'date' => today.to_s }]
+
+      expect(actual).to eq(expected)
+    end
+  end
+
+  context 'GET /holidays/year/year' do
+    it 'should return the holidays by year' do
+      date = Date.new(2017, 4, 15)
+      name = 'Holiday'
+
+      Holiday.create(name: name, occurs_at: date)
+      Holiday.create(name: 'another', occurs_at: Date.new(2009, 4, 10))
+
+      get '/holidays/year/2017'
+
+      expect(last_response.status).to eq(200)
+      actual = JSON.parse(last_response.body)
+      expected = [{ 'name' => name, 'date' => date.to_s }]
 
       expect(actual).to eq(expected)
     end
 
-    it 'two' do
-      get '/hello'
-      expect(last_response.status).to eq(200)
+    it 'should return 404 when no holidays exists in the specified year' do
+      date = Date.new(2017, 4, 15)
+      name = 'Holiday'
+
+      Holiday.create(name: name, occurs_at: date)
+
+      get '/holidays/year/2004'
+
+      expect(last_response.status).to eq(404)
       actual = JSON.parse(last_response.body)
-      expected = { 'hello' => 'world' }
+      expected = { 'error' => 'not_found', 'message' => 'No holidays found!' }
+
+      expect(actual).to eq(expected)
+    end
+
+    it 'should return 400 for invalid years' do
+      get '/holidays/year/invalid'
+
+      expect(last_response.status).to eq(400)
+      actual = JSON.parse(last_response.body)
+      message = '`invalid` is not a valid year!'
+      expected = { 'error' => 'invalid', 'message' => message }
 
       expect(actual).to eq(expected)
     end
